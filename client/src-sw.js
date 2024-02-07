4cc2749f4e68bce9836acf932cc7dbb8deba114d
Jbassard <jonathanacciarito@gmail.com>
2024-02-07 18:13:13 -0500

    this is the version I'm sticking with. Will deploy to render and add a readme later

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,60 +1,84 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+// Caching js and css requires workbox-strategies to be installed
+// To actually respond to requests with a cached response, we need to use a strategy called StaleWhileRevalidate
+// This strategy will first check the cache for a response, and if it finds one, it will return it.
+
+const { StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
-const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: "page-cache",
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
-
-warmStrategyCache({
-  urls: ["/index.html", "/"],
-  strategy: pageCache,
-});
-
+// Set up asset cache
 registerRoute(
-  ({ request }) => request.mode === "navigate",
-  ({ event }) => {
-    // Use offlineFallback for navigation requests
-    return offlineFallback({
-      pageFallback: "/index.html", // Fallback page when offline
-      event,
-    });
-  }
-);
-
-// registerRoute(({ request }) => request.mode === "navigate", pageCache);
-
-// TODO: Implement asset caching
-registerRoute(
-  /\.(?:css|js|png|jpg|jpeg|gif|svg)$/,
-  // Define CacheFirst strategy for assets
-  new CacheFirst({
-    cacheName: "assets-cache",
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: "asset-cache",
     plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 50, // Cache up to 50 assets
-        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for 7 days
       }),
     ],
   })
 );
 
+// const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+// const { CacheFirst } = require("workbox-strategies");
+// const { registerRoute } = require("workbox-routing");
+// const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+// const { ExpirationPlugin } = require("workbox-expiration");
+// const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// precacheAndRoute(self.__WB_MANIFEST);
 
+// const pageCache = new CacheFirst({
+//   cacheName: "page-cache",
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
 
+// warmStrategyCache({
+//   urls: ["/index.html", "/"],
+//   strategy: pageCache,
+// });
+
+// registerRoute(
+//   ({ request }) => request.mode === "navigate",
+//   ({ event }) => {
+//     // Use offlineFallback for navigation requests
+//     return offlineFallback({
+//       pageFallback: "/index.html", // Fallback page when offline
+//       event,
+//     });
+//   }
+// );
+
+// // registerRoute(({ request }) => request.mode === "navigate", pageCache);
+
+// // TODO: Implement asset caching
+// registerRoute(
+//   /\.(?:css|js|png|jpg|jpeg|gif|svg)$/,
+//   // Define CacheFirst strategy for assets
+//   new CacheFirst({
+//     cacheName: "assets-cache",
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//       new ExpirationPlugin({
+//         maxEntries: 50, // Cache up to 50 assets
+//         maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for 7 days
+//       }),
+//     ],
+//   })
+// );
